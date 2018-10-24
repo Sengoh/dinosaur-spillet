e3256f0fed799caa1f71d2950c65ba772e522cd8
Sengoh <sihognes@gmail.com>
2018-10-24 14:32:51 +0200

    Alt vi gjorde 24.10

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
 var myGamePiece;
-//var hekker;
+var hekker;
 var myObstacles = [];
 
+//Variabler for score
 var score = 0
+var highscore = 0
+
+var imageSrc = "https://raw.githubusercontent.com/Sengoh/dinosaur-spillet/master/bilder/";
 
 function startGame() {
-    //myGamePiece = new component(30, 30, "stein.png", 20, 250, "image");
-    myGamePiece = new component(30, 30, "red", 20, 250);
+    myGamePiece = new component(60, 60, imageSrc + "steinbilkvadrat.png?token=AqNcsw7jrF4SvivyE77QEN8cojOZR3Ioks5b2aXFwA%3D%3D", 40, 500, "image");
+    mySpookelse = new component(60,60, imageSrc + "ghost.png?token=AFEP5X8-nfBR0jZWWM6v62wnL6iJSluhks5b2aHPwA%3D%3D", 960, 200, "image");
+    //myGamePiece = new component(30, 30, "red", 20, 250);
     //myObstacles = new component(30, 60, "black", 120, 210);
     myGameArea.start();
 }
 
+// Spillområdet
 var myGameArea = {
     canvas : document.createElement("canvas"),
     start : function() {
-        this.canvas.width = 480;
-        this.canvas.height = 270;
+        this.canvas.width = 960;
+        this.canvas.height = 540;
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
         this.interval = setInterval(updateGameArea, 20);
@@ -28,7 +34,19 @@ var myGameArea = {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     }
 }
+var checkCollision = true;
+var myPowerUps = {
+  spOOkelse : function() {
+    console.log("test");
 
+    checkCollision = false;
+
+    setTimeout(function() {
+      checkCollision = true;
+    }, 5000);
+
+  }
+}
 
   var crouched = false;
   document.addEventListener("keydown", function(event) {
@@ -49,9 +67,18 @@ if(event.keyCode == 40 ) {
   crouched = false;
   myGamePiece.crouch(crouched)
 }
+if(event.keyCode == 32) {
+  myGameArea.stop()
+  startGame()
+  myObstacles = [];
+  score = 0;
+}
+
+
+
 
 })
-
+//Spillerfigur
 function component(width, height, color, x, y, type) {
     this.type = type;
     this.width = width;
@@ -64,6 +91,7 @@ function component(width, height, color, x, y, type) {
     if (type == "image") {
       this.image = new Image();
       this.image.src = color;
+      this.image.opacity = 0.5;
     }
 
     this.gravity = 0.25;
@@ -81,15 +109,16 @@ function component(width, height, color, x, y, type) {
 
         welcomeText();
 
-        text.font = "18px Arial";
+        text.font = "24px Arial";
         text.fillStyle = "red"
-        text.fillText("Score: " + score, 340, 35)
+        text.fillText("Score: " + score, 680, 70)
+        text.fillText("Highscore: " + highscore,680,95)
     }
     this.newPos = function() {
         this.gravitySpeed += this.gravity;
         this.x += this.speedX;
         this.y += this.speedY + this.gravitySpeed;
-        if(this.y < 200){
+        if(this.y < 400){
           this.gravity = 0.75
         }
         this.hitBottom();
@@ -104,45 +133,70 @@ this.gravitySpeed = -(this.gravitySpeed * this.bounce);
     this.jump = function() {
         console.log(this.y)
         // this.gravity = -0.25
-        if(this.y >= 240) {
+        if(this.type == "image") {
+            this.image.src = imageSrc + "steinbilkvadrat.png?token=AqNcsw7jrF4SvivyE77QEN8cojOZR3Ioks5b2aXFwA%3D%3D";
+        }
+        if(this.y >= 480) {
           this.gravity = -2.5
         }
-        this.height = 30;
+        this.height = 60;
     }
 
 
     this.crouch = function(crouched) {
       console.log(this.y)
       if(crouched) {
-        if(this.y == 240) {
+        if(this.y == 480) {
           if(this.type == "image") {
-              this.image.src = "stein_crouch.png";
+              this.image.src = imageSrc + "steinbilcrouch2kvadrat.png?token=AqNcs5-htIKkqYkKhKD8kc1YBxYvKJA2ks5b2aXHwA%3D%3D";
           }
-          this.height = 15;
-          this.y = this.y+15;
+          this.height = 30;
+          this.y = this.y+30;
         }
       } else if (crouched == false) {
-        this.height = 30;
+        this.height = 60;
         if(this.type == "image") {
-            this.image.src = "stein.png";
+            this.image.src = imageSrc + "steinbilkvadrat.png?token=AqNcsw7jrF4SvivyE77QEN8cojOZR3Ioks5b2aXFwA%3D%3D";
         }
       }
     }
     this.crashWith = function(otherobj) {
-      var myleft = this.x; // figur venstre
-      var myright = this.x + (this.width); //figur høyre
-      var mytop = this.y; // figur topp
-      var mybottom = this.y + (this.height); // figur bunn
-      var otherleft = otherobj.x; // obstacle  venstre
-      var otherright = otherobj.x + (otherobj.width); // obstacle høyre
-      var othertop = otherobj.y + (otherobj.height); // obstacle topp
-      var otherbottom = otherobj.y; // obstacle bunn
-      var crash = true;
-    if ((mybottom < othertop) || (mytop > otherbottom) || (myright < otherleft) || (myleft > otherright)) {
-        crash = false;
+      if(checkCollision) {
+        var myleft = this.x; // figur venstre
+        var myright = this.x + (this.width); //figur høyre
+        var mytop = this.y; // figur topp
+        var mybottom = this.y + (this.height); // figur bunn
+        var otherleft = otherobj.x; // obstacle  venstre
+        var otherright = otherobj.x + (otherobj.width); // obstacle høyre
+        var othertop = otherobj.y + (otherobj.height); // obstacle topp
+        var otherbottom = otherobj.y; // obstacle bunn
+        var crash = true;
+        if ((mybottom < othertop) || (mytop > otherbottom) || (myright < otherleft) || (myleft > otherright)) {
+          crash = false;
+        }
+        return crash;
+        //return crash;
+      }
     }
-    return crash;
-      //return crash;
+    this.pickUp = function(otherobj) {
+      if(checkCollision) {
+        var myleft = this.x; // figur venstre
+        var myright = this.x + (this.width); //figur høyre
+        var mytop = this.y; // figur topp
+        var mybottom = this.y + (this.height); // figur bunn
+        var otherleft = otherobj.x; // obstacle  venstre
+        var otherright = otherobj.x + (otherobj.width); // obstacle høyre
+        var othertop = otherobj.y; // obstacle topp
+        var otherbottom = otherobj.y +
+        (otherobj.height);; // obstacle bunn
+        var pickedup = true;
+        if ((mybottom < othertop) || (mytop > otherbottom) || (myright < otherleft) || (myleft > otherright)) {
+          pickedup = false;
+        }
+        //console.log(myright + ", " + otherleft);
+        return pickedup;
+        //return crash;
+      }
     }
 }
 
@@ -151,11 +205,39 @@ function updateGameArea() {
     myGameArea.clear();
     myGamePiece.newPos();
     myGamePiece.update();
+    if(mySpookelse != "") {
+      mySpookelse.update();
+      mySpookelse.x -= 2;
+      if (myGamePiece.pickUp(mySpookelse)) {
+        if(myGamePiece.type == "image") {
+          myGamePiece.image.style.opacity = 0.1;
+          myGamePiece.image.src = imageSrc + "steinbilkvadratopacity1.png?token=AqNcs2a1uzqH23gIz5Sy-ggbxmqcaF6gks5b2ar0wA%3D%3D";
+        }
+        console.log(myGamePiece.image.style.opacity);
+        myPowerUps.spOOkelse();
+
+          text.font = "30px Arial";
+          text.fillStyle = "red";
+          text.fillText("Boo!",200,200);
+
+        mySpookelse = "";
+      }
+    }
+    //console.log(myGamePiece.image.style.opacity);
+    if(score > highscore) {
+      highscore = score
+    }
 
     for (let i = 0; i < myObstacles.length; i += 1) {
-      myObstacles[i].x -= 3;
+      myObstacles[i].x -= 6;
       myObstacles[i].update();
-      if(myObstacles[i].x < myGamePiece.x+1.75 && myObstacles[i].x > myGamePiece.x-1.75) {
+      if(myObstacles[i].x < -40) {
+        myObstacles[i].x = -50;
+        //delete myObstacles[i];
+        //  myObstacles.splice(i,1);
+        //console.log(myObstacles[2].x);
+      }
+      if(myObstacles[i].x < myGamePiece.x+3 && myObstacles[i].x > myGamePiece.x-3) {
 
         setTimeout(function(){
           lastTry = true
@@ -165,49 +247,65 @@ function updateGameArea() {
           lastTry = false
         }
 
-
       }
     }
     for (let i = 0; i < myObstacles.length; i += 1) {
       if (myGamePiece.crashWith(myObstacles[i])) {
         myGameArea.stop();
         console.log("wat");
+
+        text.font = "30px Arial";
+        text.fillStyle = "red";
+        text.fillText("Du tapte!",200,200);
+
+        text.font = "15px Arial";
+        text.fillStyle = "red";
+        text.fillText("Vil du prøve på nytt? Trykk på space",160,240);
+
         return;
       }
     }
+    if(score >= 1000) {
+      myGameArea.canvas.style.backgroundImage = "url(" +imageSrc + "level2.png?token=AFEP5a_I4MSb3AAi4RFJ_lzo4lZSmNeCks5b2a0nwA%3D%3D)";
+    }
 }
 
-var spawnRate = 1000;
-setInterval(randomTimer, 1000)
+let spawnRate = 0;
+setInterval(randomTimer, 1899)
 function randomTimer(){
-spawnRate = (Math.floor(Math.random() * 5) + 3)*1000;
-window.setTimeout(spawnObstacle, spawnRate)
+spawnRate = (Math.floor(Math.random() * (3.2 - 2.7) + 2.7))*1000;
+// spawnRate = (Math.random() * 3.3 + 2.2)*1000;
+let test = (Math.random() * (4.3-3.2)+3.2) * 1000
+// spawnRate = (Math.floor(Math.random() * (3)) + 3);
+console.log("Spawnrate "+spawnRate);
+console.log("testytest: "+test)
+window.setTimeout(spawnObstacle, test)
 }
 
 function spawnObstacle() {
 //console.log(spawnRate);
-minWidth = 10;
-maxWidth = 60;
-minHeight = 60;
-maxHeight = 90;
+minWidth = 20;
+maxWidth = 120;
+minHeight = 120;
+maxHeight = 160;
 
-var chosenValue = Math.random() < 0.5 ? 0 : 20;
+var chosenValue = Math.random() < 0.5 ? 0 : 40;
 
 myObstacles.push(
 new component(
   20,
-  -(Math.random() * maxHeight + minHeight),
+  -(Math.random() * maxHeight + minHeight) - chosenValue,
   "blue",
-  480,
-  270 - (chosenValue)
+  960,
+  540 - (chosenValue)
 )
 )
 }
-var textX = 100;
+var textX = 200;
 function welcomeText() {
 y = 50;
 textX -= 1;
 text.font = "30px Arial";
 text.fillStyle = "red";
-text.fillText("Stein JUMPer",textX,50);
+text.fillText("Stein JUMPer",textX,100);
 }
